7c4e18bc26b621785ed34ea07f096f4ef09cb0eb
Aditya9764 <90107321+Aditya9764@users.noreply.github.com>
2023-10-02 04:12:41 +0530

    Time: 8 ms (35.15%), Space: 7.7 MB (26.41%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int start = 0 ;
+        int end = s.size() - 1 ;
+
+        while(start <= end )
+        {
+            if(isalnum(s[start])==false)
+            {
+                start++ ;
+                continue ;
+            }
+            if(isalnum(s[end])==false)
+            {
+                end-- ;
+                continue ;
+            }
+            
+            if( tolower(s[start]) != tolower(s[end]))
+            {
+                return false ;
+            }
+            else
+            {
+            start++ ;
+            end -- ;
+            }
+        }
+        return true ;
+    }
+};
