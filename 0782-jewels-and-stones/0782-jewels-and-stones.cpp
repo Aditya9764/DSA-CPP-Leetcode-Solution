2813c9b7f979def0f5d893fe0d33394398dde250
Aditya9764 <90107321+Aditya9764@users.noreply.github.com>
2023-11-09 11:58:03 +0530

    Time: 4 ms (22.21%), Space: 6.7 MB (39.73%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.cpp
+++ 0782-jewels-and-stones/0782-jewels-and-stones.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-        //itrating over first string 
+   /*     //itrating over first string 
         int count = 0 ;
     for(int i=0 ; i<jewels.size() ;i++)
     {
@@ -17,5 +17,28 @@ public:
         }
     }
     return count ;
+  */ 
+  /* 
+  int countt=0 ;
+        for (char jewel : jewels) {
+        // Count the occurrences of the current jewel character in the stone string
+        countt += count(stones.begin(), stones.end(), jewel);
     }
+     return countt ;
+  */
+  int count = 0;
+    unordered_map<char, int> mp;
+
+    for (int i = 0; i < stones.size(); i++)
+    {
+        mp[stones[i]]++;
+    }
+    for (int j = 0; j < jewels.size(); j++)
+    {
+        int val = mp[jewels[j]];
+            count +=val ;
+    }
+
+    return count ;
+  }
 };
